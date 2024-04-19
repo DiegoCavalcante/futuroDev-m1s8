b36daedfaf1e5fac71f30d88adeb6dcd4162669e
cavalcante.diego <diegosc.cavalcante@gmail.com>
2024-04-18 22:39:16 -0300

    botao adicionando elemento no body

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const title = document.getElementById("title")
+const elements = document.getElementsByClassName('text')
+
 const btnEx1 = document.getElementById('btn-ex1')
 const btnEx2 = document.getElementById('btn-ex2')
-const elements = document.getElementsByClassName('text')
+const btnEx3 = document.getElementById('btn-ex3')
+
 
 
 btnEx1.addEventListener('click',() => {
@@ -11,6 +14,12 @@ btnEx1.addEventListener('click',() => {
 btnEx2.addEventListener('click', () => {
     for(let i = 0; i<elements.length;i++){
         elements[i].innerHTML = 'Texto modificado'
-    }
-   
+    }   
 })
+
+btnEx3.addEventListener('click', () => {
+    let newElement = document.createElement('h1')
+    newElement.innerHTML = 'Elemento adicionado'
+    document.body.appendChild(newElement)
+})
+
