b804241afb69c74a6dac03894735d9422657865a
cavalcante.diego <diegosc.cavalcante@gmail.com>
2024-04-18 22:30:51 -0300

    botao modificando textos

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const title = document.getElementById("title")
-const btnH1 = document.getElementById('btn-h1')
+const btnEx1 = document.getElementById('btn-ex1')
+const btnEx2 = document.getElementById('btn-ex2')
+const elements = document.getElementsByClassName('text')
 
-btnH1.addEventListener('click',()=>{
+
+btnEx1.addEventListener('click',() => {
     title.style.backgroundColor = "blue"
 })
 
-
-
+btnEx2.addEventListener('click', () => {
+    for(let i = 0; i<elements.length;i++){
+        elements[i].innerHTML = 'Texto modificado'
+    }
+   
+})
