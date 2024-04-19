a410fc620eb826c085f49a5f987fd827c0c89f1d
cavalcante.diego <diegosc.cavalcante@gmail.com>
2024-04-18 21:59:49 -0300

    botao para mudar cor de fundo h1

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = document.getElementById("title")
+const btnH1 = document.getElementById('btn-h1')
+
+btnH1.addEventListener('click',()=>{
+    title.style.backgroundColor = "blue"
+})
+
+
+
